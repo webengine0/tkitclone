68625387b12fa1b1f768c786917f74d9053f4dd1
webengine0 <m.abdullah4757@gmail.com>
2023-09-23 09:38:59 +0500

    secon cimit

--- Components/EndContent.js
+++ Components/EndContent.js
@@ -1,6 +1,33 @@
+"use client";
+
 import React from "react";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 const EndContent = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_0d9wchm",
+        "template_c6fnqu8",
+        form.current,
+        "2-aVo2JlmoHXUnY3K"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          console.log("message sent");
+        },
+        (error) => {
+          console.log(error.text);
+          console.log("message not sent");
+        }
+      );
+  };
   return (
     <>
       <div id="scroll" className=" mt-20 mb-20">
@@ -12,16 +39,23 @@ const EndContent = () => {
             <p>Let Us know if you're looking for a digital designer.</p>
             <p>Let’s talk about the next big thing!</p>
           </div>
-          <div className="mb-10 flex justify-center border border-[#8453F9] p-1 gap-40 mt-14 2xl:gap-40 xl:gap-30 md:gap-20 sm:gap-5">
-            <input
-              type="Email"
-              placeholder="Email"
-              className="border-none outline-0 mr-2 bg-transparent ml-3 "
-            />
-            <button className="text-[#fff] bg-[#8453F9] rounded-sm p-3 ml-10 whitespace-nowrap">
-              Contact Us
-            </button>
-          </div>
+          <form action="" ref={form} onSubmit={sendEmail}>
+            <div className="mb-10 flex justify-center border border-[#8453F9] p-1 gap-40 mt-14 2xl:gap-40 xl:gap-30 md:gap-20 sm:gap-5">
+              <input
+                type="Email"
+                name="user_email"
+                placeholder="Email"
+                className="border-none outline-0 mr-2 bg-transparent ml-3 "
+              />
+              <button
+                type="submit"
+                value="Send"
+                className="text-[#fff] bg-[#8453F9] rounded-sm p-3 ml-10 whitespace-nowrap"
+              >
+                Contact Us
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </>
